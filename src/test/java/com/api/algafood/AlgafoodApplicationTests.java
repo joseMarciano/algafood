ff1235b735ada4486debd645813d0a7d60d69292
zezeka <marcianojosepaulo@gmail.com>
2020-10-01 20:52:20 -0300

    10.2. Preparando o projeto para testes de integração
    -- Test de integração vem junto no spring-boot-starter-test

--- src/test/java/com/api/algafood/AlgafoodApplicationTests.java
+++ src/test/java/com/api/algafood/AlgafoodApplicationTests.java
@@ -3,11 +3,14 @@ package com.api.algafood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class AlgafoodApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(false);
 	}
 
 }
