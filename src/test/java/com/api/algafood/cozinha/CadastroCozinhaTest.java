09ac5382187d71726f73eb0adabd46bd72b0cbd1
zezeka <marcianojosepaulo@gmail.com>
2020-10-06 08:27:57 -0300

    10.16. Desafio: refatorando o código de testes

--- src/test/java/com/api/algafood/cozinha/CadastroCozinhaTest.java
+++ src/test/java/com/api/algafood/cozinha/CadastroCozinhaTest.java
@@ -3,9 +3,9 @@ package com.api.algafood.cozinha;
 import com.api.algafood.domain.model.Cozinha;
 import com.api.algafood.domain.repository.CozinhaRepository;
 import com.api.algafood.util.DatabaseCleaner;
+import com.api.algafood.util.JsonReader;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource("/application-test.properties")
 public class CadastroCozinhaTest {
 
+    private static final Integer COZINHA_ID_INEXISTENTE = 9999999;
+
     /*
      * Injetando o RAMDOM_PORT... pois o spring vai iniciar o tomCat numa porta aleatória
      * Desse jeito eu consigo pegar a porta e jogar dentro de .port();
@@ -35,12 +37,17 @@ public class CadastroCozinhaTest {
     @Autowired
     private CozinhaRepository repository;
 
+    private Integer qntCozinhasCadastradas;
+    private Cozinha cozinhaAmericana;
+    private String jsonCorretoCozinhaChinesa;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Colocando no setup o Log de validação
         RestAssured.port = port; // Passando o port para não repetir código
         RestAssured.basePath = "/cozinhas"; //Passando o basePath para não repetir código
+
+        this.jsonCorretoCozinhaChinesa = JsonReader.read("/json/Cozinha/cadastroCozinha.json");
 
         databaseCleaner.clearTables();
         prepararDados();
@@ -58,7 +65,7 @@ public class CadastroCozinhaTest {
     }
 
     @Test
-    public void deveConter2CozinhasQuandoConsultarCozinhas() {
+    public void deveConterQuantidadeDeCozinhasCadastradasQuandoConsultarCozinhas() {
 
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
@@ -67,48 +74,50 @@ public class CadastroCozinhaTest {
                 .when()
                 .get()
                 .then()
-                .body("", Matchers.hasSize(2)) // Verifica se o corpo tem 2 itens
-                .body("nome", Matchers.hasItems("Tailandesa", "Brasileira")); //Verifica se a propriedade nome existe TAILANDESA E INDIANA
+                .body("", Matchers.hasSize(this.qntCozinhasCadastradas)); // Verifica se o corpo tem 2 itens
     }
 
     @Test
-    public void deveRetornarStatus201QuandoCadastrarCozinha(){
-            RestAssured
-                    .given()
-                    .body("{ \"nome\": \"Chinesa\" }")
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .when()
-                    .post()
-                    .then()
-                    .statusCode(HttpStatus.CREATED.value());
+    public void deveRetornarStatus201QuandoCadastrarCozinha() {
+
+
+        RestAssured
+                .given()
+                .body(this.jsonCorretoCozinhaChinesa)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 
     @Test
-    public void deveRetornarRespostaEStatusCorretosQuandoConsultarCozinhaExistente(){
+    public void deveRetornarRespostaEStatusCorretosQuandoConsultarCozinhaExistente() {
         /*
          * Dado um pathParam com nome cozinhaId = 2, aceitando JSON na resposta, quando der um get em cozinhaId
          * então, espero um statusCode de OK(200) e no corpo um "nome" igual a "Brasileira"
          */
         RestAssured
                 .given()
-                .pathParam("cozinhaId",2) // parametro da requisição com o valor de 2
+                .pathParam("cozinhaId", this.cozinhaAmericana.getId()) // parametro da requisição com o valor de 2
                 .accept(ContentType.JSON)
                 .when()
                 .get("/{cozinhaId}")
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body("nome", Matchers.equalTo("Brasileira"));
+                .body("nome", Matchers.equalTo(this.cozinhaAmericana.getNome()));
     }
+
     @Test
-    public void deveRetornarRespostaEStatus404QuandoConsultarCozinhaExistente(){
+    public void deveRetornarRespostaEStatus404QuandoConsultarCozinhaExistente() {
         /*
          * Dado um pathParam com nome cozinhaId = 100, aceitando JSON na resposta, quando der um get em cozinhaId
          * então, espero um statusCode de NOTfOUND(404)
          */
         RestAssured
                 .given()
-                .pathParam("cozinhaId",99999999) // parametro da requisição com o valor de 999999999
+                .pathParam("cozinhaId", COZINHA_ID_INEXISTENTE) // parametro da requisição com o valor de 999999999
                 .accept(ContentType.JSON)
                 .when()
                 .get("/{cozinhaId}")
@@ -116,13 +125,19 @@ public class CadastroCozinhaTest {
                 .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
-    private void prepararDados(){
-       var c1 = new Cozinha();
-       var c2 = new Cozinha();
+    private void prepararDados() {
+        this.cozinhaAmericana = cozinhaBuilderSave("Americana");
+        cozinhaBuilderSave("Tailandesa");
+        cozinhaBuilderSave("Brasileira");
+        cozinhaBuilderSave("Coreana");
 
-       c1.setNome("Tailandesa");
-       c2.setNome("Brasileira");
-       repository.save(c1);
-       repository.save(c2);
+        this.qntCozinhasCadastradas = (int) repository.count(); // pega o numero de cozinhas cadastradas no db
+    }
+
+    private Cozinha cozinhaBuilderSave(String nome) {
+        var cozinhaBuilder = new Cozinha();
+        cozinhaBuilder.setNome(nome);
+        repository.save(cozinhaBuilder);
+        return cozinhaBuilder;
     }
 }
