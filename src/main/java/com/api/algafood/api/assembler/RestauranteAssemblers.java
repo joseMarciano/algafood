4de1ea6a8f024f57afb5657db8a2452144d5c071
zezeka <marcianojosepaulo@gmail.com>
2020-10-20 23:31:14 -0300

    --Ajuste no no metodo toDomainObject

--- src/main/java/com/api/algafood/api/assembler/RestauranteAssemblers.java
+++ src/main/java/com/api/algafood/api/assembler/RestauranteAssemblers.java
@@ -2,7 +2,9 @@ package com.api.algafood.api.assembler;
 
 import com.api.algafood.api.model.representation.restaurante.RestauranteCompleta;
 import com.api.algafood.api.model.representation.restaurante.RestauranteCompletaListagem;
+import com.api.algafood.domain.model.Cozinha;
 import com.api.algafood.domain.model.Restaurante;
+import com.api.algafood.domain.repository.CozinhaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +15,23 @@ import java.util.stream.Collectors;
 public class RestauranteAssemblers implements Converter<Restaurante, RestauranteCompletaListagem, RestauranteCompleta> {
 
     private ModelMapper modelMapper;
+    private CozinhaRepository cozinhaRepository;
 
-    public RestauranteAssemblers(ModelMapper modelMapper) {
+    public RestauranteAssemblers(ModelMapper modelMapper, CozinhaRepository cozinhaRepository) {
         this.modelMapper = modelMapper;
+        this.cozinhaRepository = cozinhaRepository;
     }
 
     @Override
     public Restaurante toDomainObject(RestauranteCompleta restauranteCompleta) {
-        return modelMapper.map(restauranteCompleta, Restaurante.class);
+        /*Ajuste feito pois na hora de reotrnar o objeto, o nome da cozinha retornava null
+        * Isso deve ser provisório
+        */
+        Cozinha cozinha = cozinhaRepository.findById(restauranteCompleta.getCozinha().getId()).get();
+        Restaurante restaurante = modelMapper.map(restauranteCompleta, Restaurante.class);
+        restaurante.setCozinha(cozinha);
+
+        return restaurante;
     }
 
     @Override
